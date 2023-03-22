34634e0ba432634536ae88b92700a8957ffddf3d
bendms <97233634+bendms@users.noreply.github.com>
2023-03-22 13:57:14 +0100

    refactor views.py urls.py and models.py with PEP8 convention with Black

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,16 +4,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
-    path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
+    path("lettings/", views.lettings_index, name="lettings_index"),
+    path("lettings/<int:letting_id>/", views.letting, name="letting"),
+    path("profiles/", views.profiles_index, name="profiles_index"),
+    path("profiles/<str:username>/", views.profile, name="profile"),
+    path("admin/", admin.site.urls),
 ]
 
-#Utiliser les regex pour réduire 
-#Le linting
-#Créer 2 apps au lieu d'une seule
-#Docker regarder la doc 
-#Circle CI 
+# Utiliser les regex pour réduire
+# Le linting
+# Créer 2 apps au lieu d'une seule
+# Docker regarder la doc
+# Circle CI
