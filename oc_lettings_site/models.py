f0a76a7cd16afd155ce54ea06301b43e6144c7a4
bendms <97233634+bendms@users.noreply.github.com>
2023-03-22 13:40:16 +0100

    add plural name on address model to correct admin webpage

--- oc_lettings_site/models.py
+++ oc_lettings_site/models.py
@@ -3,13 +3,16 @@ from django.core.validators import MaxValueValidator, MinLengthValidator
 from django.contrib.auth.models import User
 
 
-class Address(models.Model):
+class Address(models.Model):        
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
     state = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
+    
+    class Meta:
+        verbose_name_plural = "address"
 
     def __str__(self):
         return f'{self.number} {self.street}'
