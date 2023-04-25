b6ee0373759bef5a9f46ec1aac08b780141baa17
bendms <97233634+bendms@users.noreply.github.com>
2023-04-25 13:48:07 +0200

    change migration 0003_auto_20230405_1437

--- oc_lettings_site/migrations/0003_auto_20230405_1437.py
+++ oc_lettings_site/migrations/0003_auto_20230405_1437.py
@@ -6,6 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
     dependencies = [
         ("oc_lettings_site", "0002_auto_20230328_1354"),
+        ("lettings", "0002_auto_20230328_1358"),
+        ("profiles", "0002_auto_20230329_1735")
     ]
 
     operations = [
