634103208d05d41f7abcb5c3a4b16819992cdd2c
bendms <97233634+bendms@users.noreply.github.com>
2023-04-26 13:19:48 +0200

    change allowed hosts

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -14,8 +14,8 @@ SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://oclettingsbdms.herokuapp.com']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'oclettingsbdms.herokuapp.com/']
+CSRF_TRUSTED_ORIGINS = ['https://oclettingsbdms.herokuapp.com/']
 # Application definition
 
 INSTALLED_APPS = [
