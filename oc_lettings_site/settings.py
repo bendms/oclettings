2a3d5eca27c1d2fe888dc7c5ded3950b5ffec7e7
bendms <97233634+bendms@users.noreply.github.com>
2023-04-27 15:05:29 +0200

    add sentry to settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -7,7 +7,6 @@ from sentry_sdk.integrations.django import DjangoIntegration
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
