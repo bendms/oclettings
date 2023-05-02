915eaba1bed00d42e1761d04f4031d5bb39fcf3e
bendms <97233634+bendms@users.noreply.github.com>
2023-05-02 15:23:02 +0200

    add environment variables with django-environ

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,3 +1,4 @@
+import environ
 import os
 
 import sentry_sdk
@@ -5,8 +6,10 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 from dotenv import load_dotenv
 
-load_dotenv()
-
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -14,11 +17,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'oclettingsbdms.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.oclettingsbdms.herokuapp.com/']
