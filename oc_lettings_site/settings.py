852b3eebaaa50d8e9dbe67b7e0bb6709f49e9d06
bendms <97233634+bendms@users.noreply.github.com>
2023-05-02 15:29:45 +0200

    deployment with environment variable in circleci

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -25,7 +25,6 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # exception if SECRET_KEY not in os.environ
 SECRET_KEY = env('SECRET_KEY')
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
