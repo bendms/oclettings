b84b185bead562c9f1b016639bdb2142ce7b7b2d
bendms <97233634+bendms@users.noreply.github.com>
2023-05-04 17:52:37 +0200

    relaunch pipeline

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -10,7 +10,7 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...) 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
